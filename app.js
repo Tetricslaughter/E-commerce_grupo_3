6438b815d4e4bcd6358017c382da88c56121c3f5
aleyufra <alejandromaryufra@gmail.com>
2023-08-08 17:54:57 -0300

    agrego la pagina de historial de compras

--- app.js
+++ app.js
@@ -12,11 +12,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"));
 })
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/register.html")); 
+    res.sendFile(path.join(__dirname, "./views/register.html"));
 })
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/login.html"));
+    res.sendFile(path.join(__dirname, "./views/login.html"));
 });
 app.get("/productCard", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/productCard.html"));
-  });
+});
+app.get("/history", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/history.html"));
+});
