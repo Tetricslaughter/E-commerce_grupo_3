29426370e89e80d276ecdc47e8eb90e0184d032f
aleyufra <alejandromaryufra@gmail.com>
2023-09-01 20:41:45 -0300

    implemento multer

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
+
+let multerDiskStorage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        let folder = path.join(__dirname, '../public/profileImages');
+        callback(null, folder);
+    },
+    filename: (req, file, callback) => {
+        let imageName = Date.now() + path.extname(file.originalname);
+        callback(null, imageName);
+    }
+})
+
+let fileUpload = multer({ storage: multerDiskStorage });
 
 const productsController = require('../controllers/productsController.js');
 
 router.get('/', productsController.list);
 router.get('/history', productsController.history);
-router.get('/create', productsController.create);
 router.get('/details/:id', productsController.details);
 router.get('/:categoria', productsController.listCategoria);
-// router.post('/', productsController.saveProduct);
+router.get('/create', productsController.create);
+router.post('/create', fileUpload.single('imagenProd'), productsController.saveProduct);
 // router.get('/:id/edit', productsController.editProduct);
 // router.put('/:id', productsController.editProduct);
 // router.delete('/:id', productsController.deleteProduct);
